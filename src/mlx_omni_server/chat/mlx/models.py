15beacc96ede3234e904bb3ac247797c5749a58e
Tao Zhu <zhutao100@gmail.com>
2025-08-24 21:18:08 -0700

    feat(chat): free memory before loading another model.

--- src/mlx_omni_server/chat/mlx/models.py
+++ src/mlx_omni_server/chat/mlx/models.py
@@ -1,6 +1,11 @@
+import gc
+from mlx.core import clear_cache
+
 from ..text_models import BaseTextModel
 from .mlx_model import MLXModel
 from .model_types import MlxModelCache, ModelId
+
+from ...utils.logger import logger
 
 # Initialize global cache objects
 _model_cache = None
@@ -23,10 +28,20 @@ def load_model(model_id: ModelId) -> BaseTextModel:
 
     if model_needs_reload:
         # Cache miss, create a new cache object
+        del _model_cache
+        del _mlx_model_cache
+        clear_cache()
+        gc.collect()
         _model_cache = MlxModelCache(model_id)
-
-        # Create and cache new MLXModel instance
         _mlx_model_cache = MLXModel(model_cache=_model_cache)
+    else:
+        if not _mlx_model_cache:
+            logger.error("Unexpected state: cached MLXModel is expected but not found.")
+            if _model_cache:
+                logger.error("Inconsistent state: cached MLXModelCache exists but MLXModel does not.")
+            # This should not happen
+            _model_cache = MlxModelCache(model_id)
+            _mlx_model_cache = MLXModel(model_cache=_model_cache)
 
     # Return cached model instance
     return _mlx_model_cache
